d8a62908815a8d41565df494fcc7473a8f0d1fb1
Lipika Arya <lipikaarya2011@gmail.com>
2024-04-17 03:09:24 -0400

    Update database.py

--- database.py
+++ database.py
@@ -1,14 +1,13 @@
 import sqlite3
+from werkzeug.security import generate_password_hash
 
-# Hosts DB into local enviroment.
 connection = sqlite3.connect("employee.db")
-# Gets connection.
 cur = connection.cursor()
-# Creates table into DB.
 cur.execute('''CREATE TABLE employeeInfo(employeeId INTEGER PRIMARY KEY AUTOINCREMENT,
                                         firstName TEXT,
                                         lastName TEXT,
-                                        password TEXT )''')
+                                        password TEXT,
+                                        authorization TEXT )''')
 cur.execute('''CREATE TABLE employeeActivity(employeeId INTEGER,
                                             date TEXT ,
                                             startTime TEXT,
@@ -20,8 +19,15 @@ cur.execute('''CREATE TABLE employeeActivity(employeeId INTEGER,
                                             newShiftAllowed TEXT,
                                             PRIMARY KEY (employeeId, date, startTime))''')
 
+admin_firstname = 'Admin'
+admin_lastname = 'Admin'
+admin_password = 'admin_password1'
+admin_authorization = 'employer'
 
+hashed_admin_password = generate_password_hash(admin_password)
 
-# Closes Connection.
+cur.execute('''INSERT INTO employeeInfo (firstName, lastName, password, authorization)
+               VALUES (?, ?, ?, ?)''', (admin_firstname, admin_lastname, hashed_admin_password, admin_authorization))
+
 connection.commit()
 connection.close()
